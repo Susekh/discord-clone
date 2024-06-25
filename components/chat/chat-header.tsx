7cb51dfba6b91549987cd4a96f11ebf12ed695d8
Subh <subhranshukhilar@outlook.com>
2024-06-25 12:55:46 +0530

    Conversations Setup + Socket IO setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,8 @@
 import { Hash } from 'lucide-react';
 import React from 'react'
 import { MobileToggle } from '../ui/mobile-toggle';
+import { UserAvatar } from '../ui/user-avatar';
+import { SocketIndicator } from '../socket-indicator';
 
 interface ChatHeaderProps {
     serverId : string;
@@ -25,9 +27,18 @@ function ChatHeader({
         {type === "channel" &&  (
             <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />
         )}
+        {type === "conversation" && (
+            <UserAvatar 
+                src={imageUrl}
+                className='h-8 w-8 md:h-8 md:w-8 mr-2'
+            />
+        )}
         <p className=' font-semibold text-md text-black dark:text-white'>
             {name}
         </p>
+        <div className='ml-auto flex items-center'>
+            <SocketIndicator />
+        </div>
     </div>
   )
 }
