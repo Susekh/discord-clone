47b6b7aa85d763e30b6d4f8ec6703fc5e09f2cfb
Subh <subhranshukhilar@outlook.com>
2024-07-03 21:51:31 +0530

    added minor changes to Badge component

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -16,7 +16,7 @@ export const SocketIndicator = () => {
 
     return (
         <Badge variant={"outline"} className="bg-emerald-600 text-white">
-            Live: Real-time updates 
+            Live
         </Badge>
     )
 }
